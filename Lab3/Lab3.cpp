3f7aa763feec9e0b80c553ef8fab51fac14a7348
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-10-14 18:26:16 +0300

    added new variable

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -2,13 +2,14 @@
 using namespace  std;
 
 int main() {
-    double x,  currentComponent;
+    double x,  currentComponent, addition;
     double e = 0.00001, functE = 1, previousComponent = 1, diff = 1;
     int n = 1;
 
     cout << "x"; cin >> x;
     while (diff >= e) {
-        currentComponent = previousComponent * (x/n);
+        addition = x / n;
+        currentComponent = previousComponent * addition;
         functE += currentComponent;
         diff = (abs(currentComponent - previousComponent));
         previousComponent = currentComponent;
