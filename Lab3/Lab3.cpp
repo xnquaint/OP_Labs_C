2e37b4c72055e65c8ea59685860fbb2e3ed93ef8
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-10-14 18:30:35 +0300

    added cout for x

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -3,10 +3,10 @@ using namespace  std;
 
 int main() {
     double x,  currentComponent, addition;
-    double e = 0.00001, functE = 1, previousComponent = 1, diff = 1;
+    double e = 0.00001, functE = 1, previousComponent = 1, diff = 1; // е - точність, functE - значення функції е
     int n = 1;
 
-    cout << "x"; cin >> x;
+    cout << "Enter x: "; cin >> x;
     while (diff >= e) {
         addition = x / n;
         currentComponent = previousComponent * addition;
