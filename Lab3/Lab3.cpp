016cdc0314f785e94d8983d0ea14778f34e1f334
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-10-21 22:30:18 +0300

    updated variables

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -2,17 +2,17 @@
 using namespace  std;
 
 int main() {
-    double x,  currentComponent, addition;
-    double e = 0.00001, functE = 1.0, previousComponent = 1.0, diff = 1.0; // е - точність, functE - значення функції е
+    double x,  CurrentComponent, addition;
+    double e = 0.00001, functE = 1.0, PreviousComponent = 1.0, diff = 1.0; // е - точність, functE - значення функції е
     int n = 1;
 
     cout << "Enter x: "; cin >> x;
     while (diff >= e) {
         addition = x / n;
-        currentComponent = previousComponent * addition;
-        functE += currentComponent;
-        diff = (abs(currentComponent - previousComponent));
-        previousComponent = currentComponent;
+        CurrentComponent = PreviousComponent * addition;
+        functE += СurrentComponent;
+        diff = (abs(CurrentComponent - PreviousComponent));
+        PreviousComponent = CurrentComponent;
         n++;
     }
     cout << functE;
