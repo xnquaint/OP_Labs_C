829d7e214091abe0532779d20ae9e7097ac101a8
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-11-04 20:49:25 +0200

    updated variables

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
 int main(){
     int n, i, component;
-    int PreviousComponent1 = 0, PreviousComponent2 = 1;
+    int previous_component1 = 0, previous_component2 = 1;
 
-    cout << "Enter the number of component: "; cin >> n;
-    if( n == 0 || n == 1){
-        cout <<"The component is: " << n;
+    cout << "Enter the number of component: " ; cin >> n;
+
+    if (n<0){
+        cout <<"You should start from 0" << endl;
     }
-    else if (n<0){
-        cout <<"You should start from 0";
+    if(n == 0 || n == 1){
+        cout <<"The component is: " << n << endl;
     }
     else {
         for (i = 1; i < n; i++) {
-            component = PreviousComponent1 + PreviousComponent2;
-            PreviousComponent1 = PreviousComponent2;
-            PreviousComponent2 = component;
+            component = previous_component1 + previous_component2;
+            previous_component1 = previous_component2;
+            previous_component2 = component;
         }
         cout << "The component is: " << component;
     }
